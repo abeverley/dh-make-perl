ada36786fac7ae9cb4ea5be70666edaa669e4d62
Gregor Herrmann <gregoa@debian.org>
2009-01-17 23:06:46 +0000

    Add the Apache License, Version 2.0 to the list of recognized licenses (closes: #511588).

--- lib/DhMakePerl.pm
+++ lib/DhMakePerl.pm
@@ -1379,6 +1379,18 @@ sub create_copyright {
             . "    option) any later version\n"
             . "    On Debian GNU/Linux systems, the complete text of version 3 of the GNU\n"
             . "    General Public License can be found in `/usr/share/common-licenses/GPL-3'",
+        'Apache-2.0' =>
+            "    Licensed under the Apache License, Version 2.0 (the \"License\");\n"
+            . "    you may not use this file except in compliance with the License.\n"
+            . "    You may obtain a copy of the License at\n"
+            . "        http://www.apache.org/licenses/LICENSE-2.0\n"
+            . "    Unless required by applicable law or agreed to in writing, software\n"
+            . "    distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+            . "    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+            . "    See the License for the specific language governing permissions and\n"
+            . "    limitations under the License.\n"
+            . "    On Debian GNU/Linux systems, the complete text of the Apache License,\n"
+            . "    Version 2.0 can be found in `/usr/share/common-licenses/Apache-2.0'",
         'unparsable' =>
             "    No known license could be automatically determined for this module.\n"
             . "    If this module conforms to a commonly used license, please report this\n"
@@ -1430,6 +1442,10 @@ sub create_copyright {
 
             if ( $mangle_cprt =~ /Artistic\s*License/is ) {
                 $licenses{'Artistic'} = 1;
+            }
+
+            if ( $mangle_cprt =~ /Apache\s*License.*2\.0/is ) {
+                $licenses{'Apache-2.0'} = 1;
             }
 
             # Other licenses?
