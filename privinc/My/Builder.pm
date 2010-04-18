d9e009a70ba4bce60006dc3330b9569e362911f4
Damyan Ivanov <dmn@debian.org>
2010-04-18 12:43:37 +0000

    hijack the dist action and disable it
    Since the primary distribution point is the Debian archive, override
    the dist ./Build action with a message explaining where to get the
    original source from.

--- privinc/My/Builder.pm
+++ privinc/My/Builder.pm
@@ -7,7 +7,7 @@ use base qw(Module::Build);
 sub ACTION_orig {
     my $self = shift;
     $self->ACTION_manifest();
-    $self->ACTION_dist();
+    $self->SUPER::ACTION_dist();
     my $dn       = $self->dist_name;
     my $ver      = $self->dist_version;
     my $pkg_name = 'dh-make-perl';
@@ -17,5 +17,23 @@ sub ACTION_orig {
     print "../$pkg_name\_$ver.orig.tar.gz ready.\n";
 }
 
-1;
+sub ACTION_dist {
+    warn <<EOF;
+The 'dist' action is usualy used to create a tar.gz to upload to CPAN.
 
+The primary distribution point of dh-make-perl is the Debian archive. If you
+need a tar.gz for CPAN, download the source tarball from Debian. `apt-get
+source --tar-only dh-make-perl' can be used for that.
+
+If you don't happen to run Debian (!), see
+http://packages.debian.org/source/unstable/dh-make-perl
+
+In case you want to upload to Debian and need and .orig.tar.gz, run the
+`orig' action.
+EOF
+
+    return 1;
+}
+
+
+1;
