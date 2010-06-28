32cf803f321afb0aae67009b57e9dbab7b072ff7
Salvatore Bonaccorso <salvatore.bonaccorso@gmail.com>
2010-06-28 05:43:07 +0000

    * Create packages with Standards-Version: 3.9.0. * Standards-Version: 3.9.0 (no changes needed)

--- lib/DhMakePerl/Command/Packaging.pm
+++ lib/DhMakePerl/Command/Packaging.pm
@@ -35,7 +35,7 @@ use Parse::DebianChangelog;
 use Text::Wrap qw(fill);
 use User::pwent;
 
-use constant debstdversion => '3.8.4';
+use constant debstdversion => '3.9.0';
 
 our %DEFAULTS = (
     start_dir => getcwd(),
