7b948c4bf71e50ded0cbc1ce3ef2037712feb343
gregor herrmann <gregoa@debian.org>
2013-02-20 19:15:15 +0100

    DhMakePerl::Command::Packaging: replace "This library"
    with the module name in the long description, like we already did with
    "This module".

--- lib/DhMakePerl/Command/Packaging.pm
+++ lib/DhMakePerl/Command/Packaging.pm
@@ -720,6 +720,7 @@ sub extract_desc {
             || '';
         ( $modulename = $self->perlname ) =~ s/-/::/g;
         $long_desc =~ s/This module/$modulename/;
+        $long_desc =~ s/This library/$modulename/;
 
         local ($Text::Wrap::columns) = 78;
         $long_desc = fill( "", "", $long_desc );
