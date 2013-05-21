f21a54b8a64578817d5e2036eef2ae4518361575
Damyan Ivanov <dmn@debian.org>
2013-05-21 23:39:34 +0300

    drop mangling versions like 2.003004 into 2.3.4
    this partially reverts 385451609f5af2ace92c3838133dd4ed2c605608 see there
    for the reasoning back in 2010 (TODO is interesting)
    
    the fix seems to cause more problems than it solves. matching cpan-version
    to debian version is dark area

--- lib/DhMakePerl/Command/Packaging.pm
+++ lib/DhMakePerl/Command/Packaging.pm
@@ -305,10 +305,6 @@ sub sanitize_version {
     return undef unless defined($ver);
 
     $ver =~ s/^v//;
-    $ver =~ s/\.(\d\d\d)(\d\d\d)/.$1.$2/;    # 2.003004 -> 2.003.004
-    $ver =~ s/\.0+(?=\d)/./g;                # 2.003.004 -> 2.3.4
-                                             # but avoid 2.0 -> 2.
-                                             #  or 2.0test -> 2.test
     $ver =~ s/[^-.+a-zA-Z0-9]+/-/g;
     $ver = "0$ver" unless $ver =~ /^\d/;
 
